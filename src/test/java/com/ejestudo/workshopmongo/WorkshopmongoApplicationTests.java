e616e8a0d7e663ec161bd22ea59718e3429447c2
Eraldo Cunha <eraldo_cunha@hotmail.com>
2023-10-15 23:46:17 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/ejestudo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejestudo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
