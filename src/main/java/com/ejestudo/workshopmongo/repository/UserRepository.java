9f1fe011665b370a9efff395a05a76b211898969
Eraldo Cunha <eraldo_cunha@hotmail.com>
2023-10-17 15:59:58 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ejestudo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ejestudo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ejestudo.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
